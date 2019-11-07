6208329b2bf844e89824fbbac8bad2b6cf3b1be2
d4tAloUh <d4tAloUh@users.noreply.github.com>
2019-11-07 10:39:24 +0200

    installed webpack

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, "docs"),
+        filename: 'js/main.js',
+    },
+    watch: true
+};
